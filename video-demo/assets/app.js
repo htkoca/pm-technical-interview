c07d5a93334469e32155665a6278bfe2186ff787
Tony Ko <tony@htko.ca>
2019-02-23 20:32:36 -0500

    Add timeout code and toggle

--- video-demo/assets/app.js
+++ video-demo/assets/app.js
@@ -1,12 +1,26 @@
+// wait for dom to be ready 
 document.addEventListener("DOMContentLoaded", function(event) { 
 
+  // init vars
+  var rainbowState = false;
+  var rainbowTimer; // if required, rainbow can timeout after 10s, just uncomment timer code below
   var $el = document.getElementsByClassName('trigger')[0];
+
+  // trigger on click
   $el.addEventListener("click", function(e){
     e.preventDefault();
-    $el.classList.add("rainbow");
-    setTimeout(function(){
+    if(!rainbowState){
+      rainbowState = true;
+      $el.classList.add("rainbow");
+      // rainbowTimer = setTimeout(function(){ 
+      //   $el.classList.remove("rainbow");
+      // }, 10000)
+    } else {
+      rainbowState = false;
       $el.classList.remove("rainbow");
-    }, 10000)
+      clearTimeout(rainbowTimer);
+    }
+    
   });
 
 });
